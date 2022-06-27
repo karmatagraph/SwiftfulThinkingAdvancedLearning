f100b4fd3661413b16b88d6ce3317b3fc599a676
karma <karmatashi954@gmail.com>
2022-06-27 17:08:08 +0545

    add of custom view modifiers

---
+++ SwiftfulThinkingAdvancedLearning/ViewModifierBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModifierBootcamp .swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by karma on 6/27/22.
+//
+
+import SwiftUI
+
+struct ViewModifierBootcamp_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewModifierBootcamp__Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModifierBootcamp_()
+    }
+}
