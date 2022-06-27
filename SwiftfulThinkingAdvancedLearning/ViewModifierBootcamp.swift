df943293338c5b0dcfab443d21c87a8228490663
karma <karmatashi954@gmail.com>
2022-06-27 17:08:21 +0545

    add of custom view modifiers

--- SwiftfulThinkingAdvancedLearning/ViewModifierBootcamp.swift
+++ SwiftfulThinkingAdvancedLearning/ViewModifierBootcamp.swift
@@ -1,5 +1,5 @@
 //
-//  ViewModifierBootcamp .swift
+//  ViewModifierBootcamp.swift
 //  SwiftfulThinkingAdvancedLearning
 //
 //  Created by karma on 6/27/22.
@@ -7,14 +7,44 @@
 
 import SwiftUI
 
-struct ViewModifierBootcamp_: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct DefaultButtonViewModifier: ViewModifier {
+    let backgroundColor: Color
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .frame(height:55)
+            .frame(maxWidth: .infinity)
+            .foregroundColor(.white)
+            .background(backgroundColor)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            .padding()
+    }
+    
+    
+}
+
+extension View {
+    func withDefaultButtonFormatting(backgroundColor: Color = .blue) -> some View {
+        modifier(DefaultButtonViewModifier(backgroundColor: backgroundColor))
     }
 }
 
-struct ViewModifierBootcamp__Previews: PreviewProvider {
+struct ViewModifierBootcamp: View {
+    var body: some View {
+        VStack {
+            Text("Hello!")
+                .withDefaultButtonFormatting()
+            Text("World!")
+                .withDefaultButtonFormatting(backgroundColor: .pink)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .withDefaultButtonFormatting(backgroundColor: .green)
+        }
+    }
+}
+
+struct ViewModifierBootcamp_Previews: PreviewProvider {
     static var previews: some View {
-        ViewModifierBootcamp_()
+        ViewModifierBootcamp()
     }
 }
